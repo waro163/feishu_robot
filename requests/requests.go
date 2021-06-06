7967944e82bbba5513a92474b34f8ba27a630f53
waro163 <waro163@163.com>
2021-06-06 14:47:32 +0800

    add requests package

---
+++ requests/requests.go
@@ -0,0 +1,66 @@
+package requests
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Body []byte
+	Code int
+}
+
+func Request(url string, method string, payload interface{}) (Response, error) {
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("marshal payload occur error: %s\n", err)
+		return Response{}, err
+	}
+	body := bytes.NewReader(data)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, body)
+
+	if err != nil {
+		return Response{}, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+
+	response, err := client.Do(req)
+	if err != nil {
+		return Response{}, err
+	}
+	defer response.Body.Close()
+
+	resBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return Response{}, err
+	}
+	// log.Printf("%d, %s", response.StatusCode, string(resBody))
+	return Response{resBody, response.StatusCode}, nil
+}
+
+func Get(url string, payload interface{}) (Response, error) {
+	return Request(url, "GET", payload)
+}
+
+func Post(url string, payload interface{}) (Response, error) {
+	return Request(url, "POST", payload)
+}
+
+func Put(url string, payload interface{}) (Response, error) {
+	return Request(url, "PUT", payload)
+}
+
+func Patch(url string, payload interface{}) (Response, error) {
+	return Request(url, "PATCH", payload)
+}
+
+func Delete(url string, payload interface{}) (Response, error) {
+	return Request(url, "DELETE", payload)
+}
