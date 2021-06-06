25951363d21a7fbc47a38669c0b99bffff8bb590
waro163 <waro163@163.com>
2021-06-06 22:51:59 +0800

    add feishu package

---
+++ feishu/token.go
@@ -0,0 +1,58 @@
+package feishu
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/spf13/viper"
+	"github.com/waro163/feishu_robot/requests"
+)
+
+//企业自建应用获取 tenant_access_token
+func GetTenantAccessToken() (string, error) {
+	url := "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/"
+	// body := struct {
+	// 	AppID     string `json:"app_id"`
+	// 	AppSecret string `json:"app_secret"`
+	// }{
+	// 	AppID:     viper.GetString("APP_ID"),
+	// 	AppSecret: viper.GetString("APP_SECRET"),
+	// }
+	body := map[string]string{
+		"app_id":     viper.GetString("APP_ID"),
+		"app_secret": viper.GetString("APP_SECRET"),
+	}
+	resp, err := requests.Post(url, body)
+	if err != nil {
+		return "", err
+	}
+	var res map[string]interface{}
+	if err = json.Unmarshal(resp.Body, &res); err != nil {
+		return "", err
+	}
+	if res["code"] != 0 {
+		return "", fmt.Errorf("code: %d, msg: %s", res["code"], res["message"])
+	}
+	return res["tenant_access_token"].(string), nil
+}
+
+// 企业自建应用获取 app_access_token
+func GetAppAccessToken() (string, error) {
+	url := "https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal/"
+	body := map[string]string{
+		"app_id":     viper.GetString("APP_ID"),
+		"app_secret": viper.GetString("APP_SECRET"),
+	}
+	resp, err := requests.Post(url, body)
+	if err != nil {
+		return "", err
+	}
+	var res map[string]interface{}
+	if err = json.Unmarshal(resp.Body, &res); err != nil {
+		return "", err
+	}
+	if res["code"] != 0 {
+		return "", fmt.Errorf("code: %d, msg: %s", res["code"], res["message"])
+	}
+	return res["app_access_token"].(string), nil
+}
