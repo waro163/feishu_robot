3a724f1d36adcb9ca3a65223b60c5a06da547ccb
waro163 <waro163@163.com>
2021-06-18 19:27:39 +0800

    add GetUserList function

--- feishu/address-book/user.go
+++ feishu/address-book/user.go
@@ -63,3 +63,29 @@ func CreateUser(token string, body map[string]interface{}, querys ...map[string]
 	}
 	return res, nil
 }
+
+// https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/contact-v3/user/list
+// token should be value of 'tenant_access_token' or 'user_access_token'
+func GetUserList(token string, querys ...map[string]string) (map[string]interface{}, error) {
+	url := "https://open.feishu.cn/open-apis/contact/v3/users"
+	if len(querys) > 0 {
+		var query_param []string
+		for key, value := range querys[0] {
+			query_param = append(query_param, key+"="+value)
+		}
+		url = url + "?" + strings.Join(query_param, "&")
+	}
+	header := map[string]string{"Authorization": "Bearer " + token}
+	resp, err := requests.Get(url, nil, header)
+	if err != nil {
+		return nil, err
+	}
+	var res map[string]interface{}
+	if err = json.Unmarshal(resp.Body, &res); err != nil {
+		return map[string]interface{}{"msg": string(resp.Body)}, err
+	}
+	if resp.Code != 200 {
+		return res, fmt.Errorf("http status: %d", resp.Code)
+	}
+	return res, nil
+}
