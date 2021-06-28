a4716c7fa2f374d341855658ebf21e13d0c2f32a
waro163 <waro163@163.com>
2021-06-28 16:40:04 +0800

    add eventmethod package to register event type to handle event msg

---
+++ event-method/event-method.go
@@ -0,0 +1,25 @@
+package eventmethod
+
+import (
+	"sync"
+)
+
+type EventFunc func(map[string]string, map[string]interface{})
+
+var EventMethod map[string]EventFunc
+var eventMethodLock = new(sync.RWMutex)
+
+func RegisterEventMethod(eventType string, f EventFunc) {
+	eventMethodLock.Lock()
+	EventMethod[eventType] = f
+	eventMethodLock.Unlock()
+}
+
+func GetEventMethod(eventType string) EventFunc {
+	eventMethodLock.RLock()
+	defer eventMethodLock.RUnlock()
+	if method, ok := EventMethod[eventType]; ok {
+		return method
+	}
+	return nil
+}
