f908fb41ca3af5186fc6bd2767d1dad18f1505e5
waro163 <waro163@163.com>
2021-07-29 11:47:33 +0800

    add message function to feishu

--- feishu/message/message.go
+++ feishu/message/message.go
@@ -1,6 +1,8 @@
 package message
 
 import (
+	"fmt"
+
 	"github.com/waro163/feishu_robot/feishu"
 )
 
@@ -21,4 +23,54 @@ func BatchSendMsg(token string, body map[string]interface{}) (map[string]interfa
 	url := "https://open.feishu.cn/open-apis/message/v4/batch_send/"
 	header := map[string]string{"Authorization": "Bearer " + token}
 	return feishu.Request("POST", url, nil, header, body)
+}
+
+// https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/reply
+func ReplyMsg(msgID, token string, body map[string]interface{}) (map[string]interface{}, error) {
+	url := fmt.Sprintf("https://open.feishu.cn/open-apis/im/v1/messages/%s/reply", msgID)
+	header := map[string]string{"Authorization": "Bearer " + token}
+	return feishu.Request("POST", url, nil, header, body)
+}
+
+// https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/delete
+func RevokeMsg(msgID, token string, body map[string]interface{}) (map[string]interface{}, error) {
+	url := fmt.Sprintf("https://open.feishu.cn/open-apis/im/v1/messages/%s", msgID)
+	header := map[string]string{"Authorization": "Bearer " + token}
+	return feishu.Request("DELETE", url, nil, header, body)
+}
+
+// https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/patch
+func UpdateMsg(msgID, token string, body map[string]interface{}) (map[string]interface{}, error) {
+	url := fmt.Sprintf("https://open.feishu.cn/open-apis/im/v1/messages/%s", msgID)
+	header := map[string]string{"Authorization": "Bearer " + token}
+	return feishu.Request("PATCH", url, nil, header, body)
+}
+
+// https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/read_users
+func QueryMsgReadStatus(msgID, token string, querys map[string]string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("https://open.feishu.cn/open-apis/im/v1/messages/%s/read_users", msgID)
+	header := map[string]string{"Authorization": "Bearer " + token}
+	return feishu.Request("GET", url, querys, header, nil)
+}
+
+// https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/list
+func GetChatHistoryMsg(token string, querys map[string]string) (map[string]interface{}, error) {
+	url := "https://open.feishu.cn/open-apis/im/v1/messages"
+	header := map[string]string{"Authorization": "Bearer " + token}
+	return feishu.Request("GET", url, querys, header, nil)
+}
+
+// https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message-resource/get
+// 返回文件的二进制流
+func GetResourceFile(msgID, fileKey, token string, querys map[string]string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("https://open.feishu.cn/open-apis/im/v1/messages/%s/resources/%s", msgID, fileKey)
+	header := map[string]string{"Authorization": "Bearer " + token}
+	return feishu.Request("GET", url, querys, header, nil)
+}
+
+// https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/get
+func GetMsgContent(msgID, token string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("https://open.feishu.cn/open-apis/im/v1/messages/%s", msgID)
+	header := map[string]string{"Authorization": "Bearer " + token}
+	return feishu.Request("GET", url, nil, header, nil)
 }
